9f450303ac87009c9098ab034f39965ab4c4926f
Unknown <fournierje.info@gmail.com>
2018-05-06 17:13:19 -0400

    Added Room cell generation, that will be used to gen. rooms
    See link in DungeonGrid.cpp. Explains the concept of creating cells that will house rooms inside, helps delimit the extents of rooms. These will be used to create actual rooms that we can then triangulate and create tunnels for connections

--- DungeonBuilder/Source/DungeonBuilder/Private/DungeonGrid.cpp
+++ DungeonBuilder/Source/DungeonBuilder/Private/DungeonGrid.cpp
@@ -3,11 +3,11 @@
 #include "DungeonGrid.h"
 #include "DrawDebugHelpers.h"
 
+DEFINE_LOG_CATEGORY(LogDungeonGrid);
+
 // Sets default values
 ADungeonGrid::ADungeonGrid()
-{
-
-}
+{}
 
 // Called when the game starts or when spawned
 void ADungeonGrid::BeginPlay()
@@ -16,29 +16,139 @@ void ADungeonGrid::BeginPlay()
 
 	const int32 AmountOfGridBlocks = GridSize * GridSize;
 		
-	for ( int32 idx = 0; idx < AmountOfGridBlocks; idx++)
-	{
-		float const GridExtent = (GridSize * GridBlockSize) / 2;
-		float const NewBlockX = (idx % GridSize) * GridBlockSize - GridExtent; 
-		float const NewBlockY = (idx / GridSize) * GridBlockSize - GridExtent;
+	// Start by creating the inital grid blocks
+	GenerateInitialGrid(AmountOfGridBlocks);
 
-		FGridBlock NewGridBlock;
-		NewGridBlock.GridBlockWorldCoordinates = FVector2D( NewBlockX, NewBlockY);				
-		GridBlocks.Add(NewGridBlock);
-	}
+	/* http://roguebasin.roguelikedevelopment.org/index.php?title=Grid_Based_Dungeon_Generator */
+
+	// Then generate room cells that will be used to place rooms in the grid
+	GenerateRoomCells(RoomCells);
+
+	// Select some room cells depending on room quantity
+	SelectRoomCells(RoomQuantity);
+
+	// Using the selected room cells create actual rooms with the grid
+
+	// Add delaunay Triangulation to select connections between actual final rooms with their extents
+
+	// Add tunnel creation algorithm here
 
 	if (bDrawDebugGrid )
 	{
-		DrawDebugGrid();
+		DrawDebugGridCellsWorld();
 	}
 	
 }
 
-void ADungeonGrid::DrawDebugGrid()
+template <typename InElementType>
+InElementType& TArray2D<InElementType>::GetAtCoordinates(const int32 X, const int32 Y)
 {
-	for (FGridBlock GridBlock : GridBlocks)
+	check(Num());
+	int32 idx = X + (Y * GridSize());
+	if (idx >= Num())
 	{
-		FColor LocalDebugColor = GridBlock.bIsOccupied ? FColor::Red : DebugColor;
+		UE_LOG(LogDungeonGrid, Error, TEXT("Bad coordinates : X = %d, Y = %d. Returning first item"), X, Y);
+		idx = 0;
+	}
+	return GetData()[idx];
+}
+
+
+void ADungeonGrid::GenerateInitialGrid(const int32 AmountOfGridBlocks)
+{
+	// Check that this is called once on a grid only
+	check(DungeonGrid.Num() == 0);
+
+	for (int32 idx = 0; idx < AmountOfGridBlocks; idx++)
+	{
+		float const GridExtent = (GridSize * GridBlockSize) / 2;
+
+		float const NewBlockX = (idx % GridSize) * GridBlockSize - GridExtent;
+		float const NewBlockY = (idx / GridSize) * GridBlockSize - GridExtent;
+
+		FGridBlock NewGridBlock;
+		NewGridBlock.GridBlockWorldCoordinates = FVector2D(NewBlockX, NewBlockY);
+		DungeonGrid.Add(NewGridBlock);
+	}
+}
+
+void ADungeonGrid::SelectRoomCells(const int32 AmountOfRooms)
+{
+	// Get random point in the grid using the size of the grid to find some points
+	// Check that the size of the grid is bigger than 2
+	check(GridSize > 2);
+
+	const int32 CellCount1D = GridSize / CellSize;
+
+	SelectedCellsPositions.Empty();
+
+	// Select 6 room cells
+	// Rinse and repeat until we have filled the grid with the amount of rooms
+	for (int i = 0; i < AmountOfRooms; i++)
+	{
+		// While we have duplicate coordinates, search for more TODO : add safety if there's not enough cells 
+		bool bIsAlreadySet = false;
+		do 
+		{
+			// Select point in the gridblocks
+			const int32 RandomX = FMath::RandRange(0, CellCount1D - 1);
+			const int32 RandomY = FMath::RandRange(0, CellCount1D - 1);
+			UE_LOG(LogDungeonGrid, Log, TEXT(" Room cell tested is  : %d, %d"), RandomX, RandomY);
+
+			SelectedCellsPositions.Add(FIntVector2D(RandomX, RandomY), &bIsAlreadySet);
+		} while (bIsAlreadySet);
+	}
+
+	// Display these room cells
+	for (const FIntVector2D& Elem : SelectedCellsPositions)
+	{
+		const FRoomCell& SelectedRoomCell = RoomCells[Elem];
+		DrawDebugRoomCell(SelectedRoomCell, FColor::Blue);
+	}	
+}
+
+void ADungeonGrid::GenerateRoomCells(TArray<FRoomCell>& RoomCells)
+{
+	RoomCells.Empty();
+
+	const int32 CellCount1D = GridSize / CellSize;
+	const int32 CellCountRemains = GridSize % CellSize;
+
+	// Uneven cell counts and grid size results
+	if (CellCountRemains > 0)
+	{
+		UE_LOG(LogDungeonGrid, Warning, TEXT("The cell count remains = %d"), CellCountRemains);
+	}
+
+	const int32 CellCount = CellCount1D * CellCount1D;
+	if (CellCount1D > 0)
+	{
+		// Iterate and create equal sized cells, that will be used to create rooms
+		UE_LOG(LogDungeonGrid, Log, TEXT("The cell count is %d, generating the cells for room creation"), CellCount);
+		for (int idx = 0; idx < CellCount; idx++)
+		{
+			const int32 MinX = (idx % CellCount1D) * CellSize;
+			const int32 MinY = (idx / CellCount1D) * CellSize;
+
+			const int32 MaxX = MinX + CellSize;
+			const int32 MaxY = MinY + CellSize;
+
+			RoomCells.Add(FRoomCell(FVector(MinX, MinY, 0), FVector(MaxX, MaxY, 0)));
+		}
+	}
+	else
+	{
+		UE_LOG(LogDungeonGrid, Error, TEXT("The cell count is %d, should be atleast 1"), CellCount);
+	}
+	DrawDebugRoomCells();
+}
+
+
+void ADungeonGrid::DrawDebugGridCellsWorld()
+{
+	for (FGridBlock GridBlock : DungeonGrid)
+	{
+		FColor const LocalDebugColor = GridBlock.DebugColor;
 		FVector const DebugPosition = FVector(GridBlock.GridBlockWorldCoordinates.X, GridBlock.GridBlockWorldCoordinates.Y, 0.f);
 		DrawDebugPoint(GetWorld(), DebugPosition, 2.f, FColor::Purple, true);
 		FBox DebugBox = FBox::BuildAABB(DebugPosition, FVector(GridBlockSize / 2, GridBlockSize / 2, 0));
@@ -46,3 +156,25 @@ void ADungeonGrid::DrawDebugGrid()
 	}
 }
 
+void ADungeonGrid::DrawDebugRoomCells()
+{
+	const int32 CellCount1D = GridSize / CellSize;
+	for (int32 idx = 0; idx < RoomCells.Num(); idx++)
+	{
+		int32 const X = (idx % CellCount1D);
+		int32 const Y = (idx / CellCount1D);
+
+		FRoomCell& SelectedRoomCell = RoomCells[FIntVector2D(X, Y)];
+		DrawDebugRoomCell(SelectedRoomCell, FColor::Green);
+	}
+}
+
+void ADungeonGrid::DrawDebugRoomCell(const FRoomCell& RoomCell, const FColor LocalDebugColor)
+{
+	const FVector Center = RoomCell.GetCenter();
+	const FVector Extent = RoomCell.GetExtent();
+
+	DrawDebugPoint(GetWorld(), Center, 2.f, FColor::Purple, true);
+	FBox DebugBox = FBox::BuildAABB(Center, FVector(GridBlockSize / 2, GridBlockSize / 2, 0));
+	DrawDebugBox(GetWorld(), Center, Extent, FQuat::Identity, LocalDebugColor, true, -1.f, 0, 1);
+}
--- DungeonBuilder/Source/DungeonBuilder/Public/DungeonGrid.h
+++ DungeonBuilder/Source/DungeonBuilder/Public/DungeonGrid.h
@@ -6,6 +6,8 @@
 #include "GameFramework/Actor.h"
 #include "DungeonGrid.generated.h"
 
+DECLARE_LOG_CATEGORY_EXTERN(LogDungeonGrid, Log, All);
+
 USTRUCT()
 struct FGridBlock
 {
@@ -26,6 +28,74 @@ struct FGridBlock
 	UPROPERTY()
 	TSubclassOf<AActor> OccupyingActor = nullptr;
 	
+	//---------------------- Debug
+	UPROPERTY(EditAnywhere, Category = "Debug")
+	FColor DebugColor = FColor::Red;
+	//---------------------- End Debug
+	
+};
+
+//class FIntVector2D : public FIntVector
+//{
+//public:
+//	FIntVector2D(const int32 X, const int32 Y)
+//		: FIntVector(X, Y, 0) {}
+//
+//	FORCEINLINE bool operator==(const FIntVector2D& Other) const
+//	{
+//		return X == Other.X && Y == Other.Y;
+//	}
+//};
+typedef FVector2D FIntVector2D;
+
+//FORCEINLINE uint32 GetTypeHash(const FIntVector2D& Vector)
+//{
+//	return FCrc::MemCrc_DEPRECATED(&Vector, sizeof(FIntVector2D));
+//}
+
+//struct FRoomCell {
+//	FRoomCell(const int32 MinX, const int32 MinY, const int32 MaxX, const int32 MaxY) 
+//	: CellStart(MinX, MinY)
+//	, CellEnd(MaxX, MaxY)
+//	{
+//	}
+//
+//	FIntVector2D CellStart, CellEnd;
+//
+//	FIntVector2D GetCenter() const
+//	{ 
+//		return FIntVector2D(((CellEnd.X - CellStart.X) / 2) + CellStart.X,
+//			((CellEnd.Y - CellStart.Y) / 2) + CellStart.Y);
+//	}
+//
+//	FIntVector GetExtent() const
+//	{
+//		return (CellEnd - CellStart) / 2;
+//	}
+//};
+typedef FBox FRoomCell;
+
+template<typename InElementType>
+class TArray2D : public TArray<InElementType>
+{
+public:
+	InElementType & operator[](const FIntVector2D& Selection)
+	{
+		return GetAtCoordinates(Selection.X, Selection.Y);
+	}
+
+	inline int32 GridSize() { return FMath::Sqrt(Num()); }
+
+private:
+
+	InElementType & operator()(const int32 X, const int32 Y)
+	{
+		return GetAtCoordinates(X, Y);
+	}
+
+	InElementType& GetAtCoordinates(const int32 X, const int32 Y);
+
+
 };
 
 UCLASS()
@@ -37,27 +107,49 @@ public:
 	// Sets default values for this actor's properties
 	ADungeonGrid();
 
-protected:
+public:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	void DrawDebugGrid();
+	
+	// Draw cells in the world 
+	void DrawDebugGridCellsWorld();
+
+	void DrawDebugRoomCells();
+
+	void DrawDebugRoomCell(const FRoomCell& RoomCell, const FColor LocalDebugColor);
 
 public:	
 
 	//---------------------- Debug
 	UPROPERTY(EditAnywhere, Category = "Debug")
-	bool bDrawDebugGrid = true;
-
-	UPROPERTY(EditAnywhere, Category = "Debug")
-	FColor DebugColor = FColor::Purple;
+	bool bDrawDebugGrid = false;
 	//---------------------- End Debug
 
 	UPROPERTY(EditAnywhere, Category = "Grid")
-	int32 GridSize = 20;
+	int32 GridSize = 40;
+
+	UPROPERTY(EditAnywhere, Category = "RoomCells")
+	int32 CellSize = 5;
 
 	UPROPERTY(EditAnywhere, Category="Grid Block")
-	float GridBlockSize = 10.f;
+	float GridBlockSize = 20.f;
+
+
+	UPROPERTY(EditAnywhere, Category = "Rooms")
+	int32 RoomQuantity = 4;
+
 
 private:
-	TArray<FGridBlock> GridBlocks;
+
+	TArray2D<FGridBlock> DungeonGrid;
+
+	TArray2D<FRoomCell> RoomCells;
+
+	TSet<FIntVector2D> SelectedCellsPositions;
+
+	void GenerateInitialGrid(const int32 AmountOfGridBlocks);
+
+	void SelectRoomCells(const int32 AmountOfRooms);
+
+	void GenerateRoomCells(TArray<FRoomCell>& RoomCells);
 };
